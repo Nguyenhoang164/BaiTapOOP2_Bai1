b36dcbbf4c138ac5515595ed244d89f76545c5b2
PC <Kamito062004@gmail.com>
2023-04-12 16:47:08 +0700

    Update_File

---
+++ Mobile.java
@@ -0,0 +1,23 @@
+public abstract class Mobile {
+   public void downloadApp(){}
+}
+class Iphone extends Mobile {
+    @Override
+    public void downloadApp(){
+        System.out.println("download app in apple store");
+    }
+}
+class Nokia extends Mobile{
+    @Override
+    public void downloadApp(){
+        System.out.println("download app in CH Play  ");
+    }
+}
+ class TestPhone{
+    public static void main(String[] args) {
+      Iphone iphone = new Iphone();
+      Nokia nokia = new Nokia();
+        iphone.downloadApp();
+        nokia.downloadApp();
+    }
+}
